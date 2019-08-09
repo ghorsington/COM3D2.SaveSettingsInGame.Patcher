7dbb1a26ef703ea74bcbdfae4a49be2ab33eebcf
ghorsington <neigh@coder.horse>
2019-08-09 23:52:37 +0300

    Add README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Fix saving game settings in COM3D2
+
+By default, COM3D2 (1.33.1) saves your settings only when you close the game.  
+Because of that, it's really easy to get yor game settings reset if the game happens to crash or if you close it unconventionally.
+
+This simply does not make sense, as many other games save the settings *when you close the settings menu*.
+
+This Sybaris patcher fixes the issue by forcing the game to save your settings when you close the settings menu.
+
+
+## How to use
+
+1. Download Sybaris for COM3D2
+
+2. Place the DLL from [releases]() into `Sybaris` folder
+
+3. Run the game
---
+++ SaveSettingsInGame.Patcher/Patcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using UnityEngine;
+
+namespace SaveSettingsInGame.Patcher
+{
+    public static class Patcher
+    {
+        public static readonly string[] TargetAssemblyNames = { "Assembly-CSharp.dll" };
+
+        public static void Patch(AssemblyDefinition ad)
+        {
+            var md = ad.MainModule;
+            var methodDef = md.GetType("ConfigMgr").Methods.FirstOrDefault(m => m.Name == "CloseConfigPanel");
+            var ins = methodDef.Body.Instructions.Last();
+            var il = methodDef.Body.GetILProcessor();
+            il.InsertBefore(ins, il.Create(OpCodes.Call, md.ImportReference(typeof(Hooks).GetMethod(nameof(Hooks.SaveConfig)))));
+        }
+    }
+
+    public static class Hooks
+    {
+        public static void SaveConfig()
+        {
+            Debug.Log("Saving settings");
+            GameMain.Instance.CMSystem.SaveIni();
+        }
+    }
+}
