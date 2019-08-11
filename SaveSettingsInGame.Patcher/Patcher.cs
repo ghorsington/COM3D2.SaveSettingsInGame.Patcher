7395f28884239eafdcf72d661b9be59141f3ecde
ghorsington <neigh@coder.horse>
2019-08-11 11:27:58 +0300

    Save system data as well

--- SaveSettingsInGame.Patcher/Patcher.cs
+++ SaveSettingsInGame.Patcher/Patcher.cs
@@ -16,11 +16,27 @@ namespace SaveSettingsInGame.Patcher
             var ins = methodDef.Body.Instructions.Last();
             var il = methodDef.Body.GetILProcessor();
             il.InsertBefore(ins, il.Create(OpCodes.Call, md.ImportReference(typeof(Hooks).GetMethod(nameof(Hooks.SaveConfig)))));
+
+            methodDef = md.GetType("CMSystem").Methods.FirstOrDefault(m => m.Name == "SetEditColorPresetData");
+            ins = methodDef.Body.Instructions.Last();
+            il = methodDef.Body.GetILProcessor();
+            il.InsertBefore(ins, il.Create(OpCodes.Call, md.ImportReference(typeof(Hooks).GetMethod(nameof(Hooks.SaveSystemConfig)))));
+
+            methodDef = md.GetType("CMSystem").Methods.FirstOrDefault(m => m.Name == "SetSystemVers");
+            ins = methodDef.Body.Instructions.Last();
+            il = methodDef.Body.GetILProcessor();
+            il.InsertBefore(ins, il.Create(OpCodes.Call, md.ImportReference(typeof(Hooks).GetMethod(nameof(Hooks.SaveSystemConfig)))));
         }
     }
 
     public static class Hooks
     {
+        public static void SaveSystemConfig()
+        {
+            Debug.Log("Saving system settings");
+            GameMain.Instance.CMSystem.SaveSystem();
+        }
+
         public static void SaveConfig()
         {
             Debug.Log("Saving settings");
